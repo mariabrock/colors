3591b7f34fec8246185d8da17366f4c700c1aebb
mariabrock <mariabrock414@gmail.com>
2019-10-14 21:42:51 -0500

    made colors cards print

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,12 @@
 import 'bootstrap';
 import '../styles/main.scss';
-import arrayMethods from './arrayMethods';
+import cardList from './components/cardList/cardList';
+// import arrayMethods from './arrayMethods';
 
 const init = () => {
-  arrayMethods.init();
+  // arrayMethods.init();
+  // cardList.cardListEvents();
+  cardList.cardListBuilder();
 };
 
 init();
